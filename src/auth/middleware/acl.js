dd5720a0d732161676b0659d4da619be041f4bae
Robert Ball <bobball89@gmail.com>
2022-09-29 03:21:51 +0000

    added readme documentation

--- src/auth/middleware/acl.js
+++ src/auth/middleware/acl.js
@@ -1,20 +1,14 @@
 'use strict';
 
-module.exports = (capability) => {
-
-  return (req, res, next) => {
-
-    try {
-      if (req.user.capabilities.includes(capability)) {
-        next();
-      }
-      else {
-        next('Access Denied');
-      }
-    } catch (e) {
-      next('Invalid Login');
+module.exports = (capability) => (req, res, next) => {
+  try {
+    console.log('from acl middleware', req.user);
+    if(req.user.capabilities.includes(capability)){
+      next();
+    } else {
+      next('Access Denied');
     }
-
-  };
+  } catch (e){
+    next('Invalid ACL Login');
+  }
 };
-
--- src/auth/middleware/bearer.js
+++ src/auth/middleware/bearer.js
@@ -4,17 +4,15 @@ const { users } = require('../models/users');
 
 module.exports = async (req, res, next) => {
   try {
-    if (!req.headers.authorization) { _authError() }
+    if (!req.headers.authorization) { next('Invalid Login'); }
     const token = req.headers.authorization.split(' ').pop();
     const validUser = await users.authenticateToken(token);
     req.user = validUser;
     req.token = validUser.token;
     next();
   } catch (e) {
-    _authError();
-  }
-  function _authError() {
-    next('Invalid Login');
+    console.error(e);
+    res.status(403).send('Invalid Login');
   }
 };
 
